a1372863501594db6582a706606ff70fb8cf6d85
Julie De Witte <julie.dewitte@student.hogent.be>
2024-10-11 15:19:16 +0200

    script todo push

---
+++ TODO.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ "$(docker ps -a --filter 'name=TODO' -q)" ]; then
+    docker stop TODO
+    docker rm TODO
+fi
+
+cd getting-started-master/app
+
+docker build -t getting-started .
+docker run -dp 3000:3000 --name TODO getting-started
